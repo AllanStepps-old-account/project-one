3c6c1844735910a9ed4af45862e329dfadff0339
Allan STEPCZYNSKI <allan.stepczynski@amadeus.com>
2020-10-26 23:58:40 +0100

    feat(login): jwt capabilities

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,11 @@
 import {environment} from '../../environments/environment';
 import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
-import {map} from 'rxjs/operators';
+import {catchError, map, mergeMap} from 'rxjs/operators';
 import {User} from '../models/user.model';
-import {Observable, throwError} from 'rxjs';
+import {Observable, of, throwError} from 'rxjs';
+import {AuthResponse} from '../models/auth.response';
+import {parseJwt} from '../helpers/jwt.helper';
 
 @Injectable({
   providedIn: 'root'
@@ -15,23 +17,35 @@ export class UserService {
   constructor(private httpClient: HttpClient) {
   }
 
+  /**
+   * This method is server dependant since the json-server-auth
+   * only returns the sub (id) and the email of the User.
+   * @param email
+   * @param password
+   */
   login(email: string, password: string): Observable<User> {
-    return this.httpClient.get(this.path, {
-      params: {
-        email,
-        password
-      }
-    }).pipe(map((results: User[]) => {
-      if (results.length == 1) {
-        return results[0]; // @todo create a middleware for json-server
-      } else {
-        throw throwError('No user found');
-      }
-    }));
+    return this.httpClient.post(environment.apiUrl + '/login', {
+      email,
+      password
+    }).pipe(
+      map(({accessToken}: AuthResponse) => {
+        const {sub} = parseJwt(accessToken);
+        return sub;
+      }),
+      mergeMap((sub: string) => this.getUser(sub)),
+      catchError((error) => throwError(error.error))
+    );
   }
 
   signUp(user: User): Observable<User> {
-    return this.httpClient.post<User>(this.path, {...user});
+    return this.httpClient.post<User>(this.path, {...user}).pipe(
+      catchError((error) => throwError(error.error))
+    );
   }
 
+  private getUser(id: string): Observable<User> {
+    return this.httpClient.get<User>(this.path + '/' + id).pipe(
+      catchError((error) => throwError(error.error))
+    );
+  }
 }
